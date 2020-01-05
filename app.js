7671fefa2f2dedac184b26f9302bcf97c955ac19
Amritpal Singh <singhamritpal49@gmail.com>
2020-01-05 01:11:03 -0500

    added yarg boy

--- app.js
+++ app.js
@@ -1,10 +1,53 @@
 const chalk = require('chalk');
 const getNotes = require('./notes')
+const yargs = require('yargs')
 
-const command = process.argv[2]
-console.log(process.argv)
-if (command === 'add') {
-    console.log("added notes")
-} else if (command === 'remove') {
-    console.log("Removing Note")
-}
+yargs.command({
+    command: 'add',
+    describe: "add a note",
+    builder: {
+        title: {
+            describe: "Note title",
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            description: "Note Description",
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    
+    handler: function (argv) {
+        console.log("Title: "+ argv.title)
+        console.log("Body: "+ argv.body)
+    }
+
+})
+yargs.command({
+    command: 'remove',
+    describe: "remove a note",
+    handler: function () {
+        console.log("removing a note")
+    }
+})
+yargs.command({
+    command: 'list',
+    describe: "view all notes",
+    handler: function () {
+        console.log("Listing new notes")
+    }
+
+})
+yargs.command({
+    command: 'read',
+    describe: "Read note",
+    handler: function () {
+        console.log("Reading Note")
+    }
+
+})
+
+
+
+yargs.parse()
